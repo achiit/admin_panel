b6e463c2f1f19e0e9c1ea168404d3c16ac51f8ec
achintya <as4656@srmist.edu.in>
2023-03-30 20:32:25 +0530

    four blocks added

--- lib/pages/dashboard.dart
+++ lib/pages/dashboard.dart
@@ -15,7 +15,255 @@ class DashboardScreen extends StatelessWidget {
     return SafeArea(
       child: SingleChildScrollView(
         padding: EdgeInsets.all(defaultPadding),
-        child: TopNavBar(),
+        child: Column(
+          children: [
+            TopNavBar(),
+            SizedBox(
+              height: (defaultPadding * 2) - 4,
+            ),
+            Row(
+              children: [
+                Expanded(
+                  flex: 1,
+                  child: Container(
+                    height: 116,
+                    decoration: BoxDecoration(
+                      color: lightGrey,
+                      borderRadius: BorderRadius.all(Radius.circular(10)),
+                    ),
+                    child: Row(
+                      children: [
+                        SizedBox(
+                          width: 28,
+                          height: 116,
+                        ),
+                        SizedBox(
+                          height: 60,
+                          width: 60,
+                          child: Row(
+                            children: [
+                              Image.asset("assets/icons/first_box.png")
+                            ],
+                          ),
+                        ),
+                        SizedBox(
+                          width: 23,
+                          height: 116,
+                        ),
+                        Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.only(left: 0.0),
+                              child: Text(
+                                '178+',
+                                textAlign: TextAlign.left,
+                                style: GoogleFonts.dmSans(
+                                  color: Colors.white,
+                                  fontSize: 22,
+                                  fontWeight: FontWeight.w700,
+                                ),
+                              ),
+                            ),
+                            Text(
+                              'Save Products',
+                              //textAlign: TextAlign.center,
+                              style: GoogleFonts.dmSans(
+                                color: Colors.white,
+                                fontSize: 14,
+                                fontWeight: FontWeight.w400,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  width: defaultPadding,
+                ),
+                Expanded(
+                  flex: 1,
+                  child: Container(
+                    height: 116,
+                    decoration: BoxDecoration(
+                      color: lightGrey,
+                      borderRadius: BorderRadius.all(Radius.circular(10)),                      
+                    ),
+                    child: Row(
+                      children: [
+                        SizedBox(
+                          width: 28,
+                          height: 116,
+                        ),
+                        SizedBox(
+                          height: 60,
+                          width: 60,
+                          child: Row(
+                            children: [
+                              Image.asset("assets/icons/second_box.png")
+                            ],
+                          ),
+                        ),
+                        SizedBox(
+                          width: 23,
+                          height: 116,
+                        ),
+                        Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.only(left: 0.0),
+                              child: Text(
+                                '20+',
+                                textAlign: TextAlign.left,
+                                style: GoogleFonts.dmSans(
+                                  color: Colors.white,
+                                  fontSize: 22,
+                                  fontWeight: FontWeight.w700,
+                                ),
+                              ),
+                            ),
+                            Text(
+                              'Stock Products',
+                              //textAlign: TextAlign.center,
+                              style: GoogleFonts.dmSans(
+                                color: Colors.white,
+                                fontSize: 14,
+                                fontWeight: FontWeight.w400,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  width: defaultPadding,
+                ),
+                Expanded(
+                  flex: 1,
+                  child: Container(
+                    height: 116,
+                    decoration: BoxDecoration(
+                      color: lightGrey,
+                      borderRadius: BorderRadius.all(Radius.circular(10)),
+                    ),
+                    child: Row(
+                      children: [
+                        SizedBox(
+                          width: 28,
+                          height: 116,
+                        ),
+                        SizedBox(
+                          height: 60,
+                          width: 60,
+                          child: Row(
+                            children: [
+                              Image.asset("assets/icons/third_box.png")
+                            ],
+                          ),
+                        ),
+                        SizedBox(
+                          width: 23,
+                          height: 116,
+                        ),
+                        Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.only(left: 0.0),
+                              child: Text(
+                                '190+',
+                                textAlign: TextAlign.left,
+                                style: GoogleFonts.dmSans(
+                                  color: Colors.white,
+                                  fontSize: 22,
+                                  fontWeight: FontWeight.w700,
+                                ),
+                              ),
+                            ),
+                            Text(
+                              'Sales Products',
+                              //textAlign: TextAlign.center,
+                              style: GoogleFonts.dmSans(
+                                color: Colors.white,
+                                fontSize: 14,
+                                fontWeight: FontWeight.w400,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  width: defaultPadding,
+                ),
+                Expanded(
+                  flex: 1,
+                  child: Container(
+                    height: 116,
+                    decoration: BoxDecoration(
+                      color: lightGrey,
+                      borderRadius: BorderRadius.all(Radius.circular(10)),
+                    ),
+                    child: Row(
+                      children: [
+                        SizedBox(
+                          width: 28,
+                          height: 116,
+                        ),
+                        SizedBox(
+                          height: 60,
+                          width: 60,
+                          child: Row(
+                            children: [
+                              Image.asset("assets/icons/fourth_box.png")
+                            ],
+                          ),
+                        ),
+                        SizedBox(
+                          width: 23,
+                          height: 116,
+                        ),
+                        Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.only(left: 0.0),
+                              child: Text(
+                                '12+',
+                                textAlign: TextAlign.left,
+                                style: GoogleFonts.dmSans(
+                                  color: Colors.white,
+                                  fontSize: 22,
+                                  fontWeight: FontWeight.w700,
+                                ),
+                              ),
+                            ),
+                            Text(
+                              'Job Applications',
+                              //textAlign: TextAlign.center,
+                              style: GoogleFonts.dmSans(
+                                color: Colors.white,
+                                fontSize: 14,
+                                fontWeight: FontWeight.w400,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
